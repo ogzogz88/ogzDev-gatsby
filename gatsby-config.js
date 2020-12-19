3d66694beba7657fdc8ff6472f9b128c0a2d28f6
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-12-19 19:23:49 +0300

    add gatsby-plugin-sharp configuration to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,17 @@ module.exports = {
         display: 'swap'
       },
     },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+        failOnError: true,
+      },
+    },
   ],
 }
