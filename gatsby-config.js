0c9fd4676b7098fa6c297303b03c510213722609
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-12-19 07:10:47 +0300

    modify gatsby-config.js gatsby-source-strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,9 +28,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.DEPLOY_URL
-          ? "https://agile-hollows-45221.herokuapp.com"
-          : "http://localhost:1337",
+        apiURL:
+          "https://agile-hollows-45221.herokuapp.com"
+          || "http://localhost:1337",
         queryLimit: 1000,
         // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
