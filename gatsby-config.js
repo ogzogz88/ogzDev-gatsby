efca79de3347a90b695e5ba78a0cacae2bec02ed
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-12-19 19:27:24 +0300

    add forceBase64Format propert to gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
       options: {
         // Available options and their defaults:
         base64Width: 20,
-        forceBase64Format: ``, // valid formats: png,jpg,webp
+        forceBase64Format: `webp`, // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
         defaultQuality: 50,
