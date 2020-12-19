f92809a526b9cf83807ab61d2bb74a981fcdac45
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-12-19 21:02:45 +0300

    gatsby clean

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
       options: {
         // Available options and their defaults:
         base64Width: 20,
-        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        forceBase64Format: `png`, // valid formats: png,jpg,webp  
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
         defaultQuality: 50,
